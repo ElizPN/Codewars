7863ae35114d9847a6ba2c2ffe49c459970007c8
ElizPN <pankevych.liza@gmail.com>
2022-08-05 20:37:48 +0200

    fix hardcode

--- smallest_integer.js
+++ smallest_integer.js
@@ -1,13 +1,29 @@
-const arr = [34, 15, 88, 2];
+const arr = [78, 56, 232, 12, 8];
 
-function findSmallestInt(args) {
-  let smallest = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < smallest) {
-      smallest = arr[i];
+// function findSmallestInt(args) {
+//   let smallest = args[0];
+//   for (let i = 0; i < args.length; i++) {
+//     if (args[i] < smallest) {
+//       smallest = args[i];
+//     }
+//   }
+//   return smallest;
+// }
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let smallest = args[0];
+    for (let i = 0; i < args.length; i++) {
+      if (args[i] < smallest) {
+        smallest = args[i];
+      }
     }
+    return smallest;
   }
-  return smallest;
 }
 
-console.log(findSmallestInt(arr));
+const smallestFainder = new SmallestIntegerFinder();
+
+console.log(smallestFainder.findSmallestInt(arr));
+
+// console.log(findSmallestInt(arr));
