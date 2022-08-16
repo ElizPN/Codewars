7c76962886435ac2e5efdd4b25455e8d57d3ce04
ElizPN <pankevych.liza@gmail.com>
2022-08-16 21:32:17 +0200

    sum of two smallest numbers

--- sum_of_two_smallest_num.js
+++ sum_of_two_smallest_num.js
@@ -1,16 +1,20 @@
 const arrNum = [5, 8, 12, 19, 22];
+// does not work if firs element - lowest
+// function sumTwoSmallestNumbers(numbers) {
+//   let lowestNum = numbers[0];
+//   let secondLowestNum = numbers[0];
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] < lowestNum) {
+//       lowestNum = numbers[i];
+//     } else if (numbers[i] < secondLowestNum) {
+//       secondLowestNum = numbers[i];
+//     }
+//   }
+//   return lowestNum + secondLowestNum;
+// }
 
 function sumTwoSmallestNumbers(numbers) {
-  let lowestNum = numbers[0];
-  let secondLowestNum = numbers[0];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] < lowestNum) {
-      secondLowestNum = lowestNum;
-      lowestNum = numbers[i];
-
-      //   console.log(secondLowestNum);
-    }
-  }
-  return lowestNum + secondLowestNum;
+  numbers.sort((a, b) => a - b);
+  return numbers[0] + numbers[1];
 }
 console.log(sumTwoSmallestNumbers(arrNum));
