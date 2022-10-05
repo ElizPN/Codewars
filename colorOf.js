a3f7609ed2521cc2e0ed66d9936194cb2b64a398
ElizPN <pankevych.liza@gmail.com>
2022-10-05 14:32:50 +0200

    refactor

--- colorOf.js
+++ colorOf.js
@@ -1,29 +1,22 @@
 function colorOf(r, g, b) {
-  let rSexteen = "";
-  if (r < 16) {
-    rSexteen = 0 + r.toString(16);
-  } else {
-    rSexteen = r.toString(16);
-  }
-
-  let gSexteen = "";
-  if (g < 16) {
-    gSexteen = 0 + g.toString(16);
-  } else {
-    gSexteen = g.toString(16);
-  }
-
-  let bSexteen = "";
-  if (b < 16) {
-    bSexteen = 0 + b.toString(16);
-  } else {
-    bSexteen = b.toString(16);
-  }
+  let rSexteen = convertTo16(r);
+  let gSexteen = convertTo16(g);
+  let bSexteen = convertTo16(b);
 
   return "#" + rSexteen + gSexteen + bSexteen;
 }
 
 console.log(colorOf(255, 0, 0));
+
+function convertTo16(color) {
+  let colorSexteen = "";
+  if (color < 16) {
+    colorSexteen = 0 + color.toString(16);
+  } else {
+    colorSexteen = color.toString(16);
+  }
+  return colorSexteen;
+}
 // for (let index = 0; index < 100; index++) {
 //   console.log(index, index.toString(16));
 // }
