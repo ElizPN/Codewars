0763bf0b72c8cd7727b0171250a93ea5350a46cf
ElizPN <pankevych.liza@gmail.com>
2023-02-04 17:07:06 +0100

    my solution of "covfefe" kata

---
+++ covfefe.js
@@ -0,0 +1,15 @@
+function covfefe(str) {
+  let newString = "";
+  if (str.includes("coverage")) {
+    const arrWithoutCoverage = str.split("coverage");
+    newString = arrWithoutCoverage.join("covfefe");
+  } else {
+    newString = str + " covfefe";
+  }
+
+  return newString;
+}
+
+console.log(covfefe("double space "));
+
+// should add check for double space
