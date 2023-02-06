634a903878f3da9ba4a9030fb433f5e8bf1860e8
ElizPN <pankevych.liza@gmail.com>
2023-02-06 15:15:57 +0100

    same solution

--- add_length.js
+++ add_length.js
@@ -1,17 +1,3 @@
-// function addLength(str) {
-//  const arrStr = str.split(" ")
-
-//  let newArrStr = []
-
-// for (let i = 0; i < arrStr.length; i++) {
-//     const lenghtStr = arrStr[i].length
-//     newArrStr.push(arrStr[i] + " " + lenghtStr);
-// }
-
-//  return newArrStr;
-// }
-
-// console.log(addLength("apple ban"));
 
 function addLength(str) {
   const arrStr = str.split(" ");
