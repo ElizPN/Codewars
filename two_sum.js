d6c3f6e2b8edcdc60b3f10ac8dd0e4fcd6f8c295
ElizPN <pankevych.liza@gmail.com>
2023-03-13 15:10:07 +0100

    change solution

--- two_sum.js
+++ two_sum.js
@@ -4,22 +4,45 @@
 //  * @return {number[]}
 //  */
 
-const arr = [1, 2, 7, 11, 15];
-const t = 9;
-const output = [0, 1];
+// const arr = [1, 2, 7, 11, 15];
+// const myTarget = 9;
+// const output = [0, 1];
+
+// const twoSum = function (nums, target) {
+//   let firstIndex = 0;
+//   let secondIndex = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] <= target) {
+//       firstIndex = i;
+//       const difference = target - nums[i];
+//       secondIndex = nums.indexOf(difference);
+//     }
+//   }
+//   return [firstIndex, secondIndex];
+// };
+
+// const arr = [-1, -2, -3, -4, -5];
+// const myTarget = -8;
+const arr = [-1, -2, -3, -4, -5];
+const myTarget = -8;
+
+
 
 const twoSum = function (nums, target) {
   let firstIndex = 0;
   let secondIndex = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] <= target) {
+    const difference = target - nums[i];
+
+    secondIndex = nums.indexOf(difference);
+    if (secondIndex !== -1) {
       firstIndex = i;
-      const difference = target - nums[i];
-      secondIndex = nums.indexOf(difference);
+      return [firstIndex, secondIndex];
     }
+
   }
-  return [firstIndex, secondIndex];
 };
 
-console.log(twoSum(arr, t));
+console.log(twoSum(arr, myTarget));
