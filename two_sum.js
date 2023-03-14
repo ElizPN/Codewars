a744abc34ef49266a7c63b21a8284c93742e46fe
ElizPN <pankevych.liza@gmail.com>
2023-03-14 10:47:39 +0100

    finish leetCode task

--- two_sum.js
+++ two_sum.js
@@ -1,33 +1,8 @@
-// /**
-//  * @param {number[]} nums
-//  * @param {number} target
-//  * @return {number[]}
-//  */
-
-// const arr = [1, 2, 7, 11, 15];
-// const myTarget = 9;
-// const output = [0, 1];
-
-// const twoSum = function (nums, target) {
-//   let firstIndex = 0;
-//   let secondIndex = 0;
-
-//   for (let i = 0; i < nums.length; i++) {
-//     if (nums[i] <= target) {
-//       firstIndex = i;
-//       const difference = target - nums[i];
-//       secondIndex = nums.indexOf(difference);
-//     }
-//   }
-//   return [firstIndex, secondIndex];
-// };
-
 // const arr = [-1, -2, -3, -4, -5];
 // const myTarget = -8;
-const arr = [-1, -2, -3, -4, -5];
-const myTarget = -8;
 
-
+const arr = [3, 3];
+const myTarget = 6;
 
 const twoSum = function (nums, target) {
   let firstIndex = 0;
@@ -39,9 +14,10 @@ const twoSum = function (nums, target) {
     secondIndex = nums.indexOf(difference);
     if (secondIndex !== -1) {
       firstIndex = i;
-      return [firstIndex, secondIndex];
+      if (secondIndex !== firstIndex) {
+        return [firstIndex, secondIndex];
+      }
     }
-
   }
 };
 
